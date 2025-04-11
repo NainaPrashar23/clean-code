4f8e18ac5418a48e5b0d3708348ce5844eb8f61d
Naina Prashar <naina.prashar@olx.in>
2025-04-11 19:37:33 +0530

    clean code for problem2

---
+++ src/main/java/org/example/basic/FlightArrivalProcessor.java
@@ -0,0 +1,24 @@
+package org.example.basic;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class FlightArrivalProcessor {
+
+    private static final String STATUS_DELAYED = "Delayed";
+    private static final String NEW_LINE = "\n";
+
+    public String generateArrivalReport(List<LocalDate> arrivalDates) {
+        StringBuilder report = new StringBuilder();
+
+        for (LocalDate arrivalDate : arrivalDates) {
+            if (arrivalDate.isAfter(LocalDate.now())) {
+                report.append(arrivalDate).append(NEW_LINE);
+            }
+            else {
+                report.append(STATUS_DELAYED).append(NEW_LINE);
+            }
+        }
+        return report.toString();
+    }
+}

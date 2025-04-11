be40a626d60e7a3b2cd3c6c7c726ad4a21eb3923
Naina Prashar <naina.prashar@olx.in>
2025-04-11 20:02:56 +0530

    clean code for olxApp class

--- src/main/java/org/example/exercises/OlxApp.java
+++ src/main/java/org/example/exercises/OlxApp.java
@@ -3,91 +3,98 @@ package org.example.exercises;
 import java.util.Arrays;
 
 public class OlxApp {
-	
-	public Item[] items;
+
+	private static final String ITEM_AGED_WINE = "Aged Wine";
+	private static final String ITEM_BACKSTAGE_PASS = "ColdPlay concert passes";
+	private static final String ITEM_LEGENDARY = "PokemonGo";
+
+	private final Item[] items;
 
 	public OlxApp(Item[] items) {
 		this.items = items;
 	}
 
 	public static void main(String[] args) {
-
-		Item[] items = new Item[] { 
-							new Item("Mangoes", 10, 20)
-					   };
+		Item[] items = new Item[] {
+				new Item("Mangoes", 10, 20)
+		};
 
 		OlxApp app = new OlxApp(items);
-
 		app.updateQuality();
-
 		System.out.println(app);
-
 	}
 
 	public void updateQuality() {
-		for (int i = 0; i < items.length; i++) {
-			if (items[i].name != "Aged Wine" && items[i].name != "ColdPlay concert passes") {
-				if (items[i].quality > 0) {
-					if (items[i].name != "PokemonGo") {
-						items[i].quality = items[i].quality - 1;
-					}
-				}
-			} else {
-				// This part handles the items for which quality can increase
-				// "ColdPlay concert passes" and "Aged Wine"
-
-				if (items[i].quality < 50) {
-					items[i].quality = items[i].quality + 1;
-
-					if (items[i].name == "ColdPlay concert passes") {
-						if (items[i].sellIn < 11) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
-
-						if (items[i].sellIn < 6) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
-					}
-				}
-			}
-
-			// Everything except for Sulfuras the sellIn Decreases
-			if (items[i].name != "PokemonGo") {
-				items[i].sellIn = items[i].sellIn - 1;
-			}
-
-			if (items[i].sellIn < 0) {
-				if (items[i].name != "Aged Wine") {
-
-					if (items[i].name != "ColdPlay concert passes") {
-						if (items[i].quality > 0) {
-							if (items[i].name != "PokemonGo") {
-								items[i].quality = items[i].quality - 1;
-							}
-						}
-					} else {
-						// For Backstage passes with sellin less than zero
-						// quality is set to zero
-						items[i].quality = 0;
-					}
-				} else {
-					// For Aged Wine below 50 quality increases actually by 2
-					// In the previous line
-					if (items[i].quality < 50) {
-						items[i].quality = items[i].quality + 1;
-					}
-				}
-			}
+		for (Item item : items) {
+			updateItem(item);
 		}
+	}
+
+	private void updateItem(Item item) {
+		switch (item.getName()) {
+			case ITEM_AGED_WINE:
+				updateAgedWine(item);
+				break;
+			case ITEM_BACKSTAGE_PASS:
+				updateBackstagePass(item);
+				break;
+			case ITEM_LEGENDARY:
+				break;
+			default:
+				updateRegularItem(item);
+				break;
+		}
+
+		if (!item.getName().equals(ITEM_LEGENDARY)) {
+			item.setSellIn(item.getSellIn() - 1);
+		}
+
+		if (item.getSellIn() < 0) {
+			handleExpiredItem(item);
+		}
+	}
+
+	private void updateAgedWine(Item item) {
+		increaseQuality(item, 1);
+	}
+
+	private void updateBackstagePass(Item item) {
+		increaseQuality(item, 1);
+		if (item.getSellIn() <= 10) increaseQuality(item, 1);
+		if (item.getSellIn() <= 5) increaseQuality(item, 1);
+	}
+
+	private void updateRegularItem(Item item) {
+		if (item.getQuality() > 0) {
+			item.setQuality(item.getQuality() - 1);
+		}
+	}
+
+	private void handleExpiredItem(Item item) {
+		switch (item.getName()) {
+			case ITEM_AGED_WINE:
+				increaseQuality(item, 1);
+				break;
+			case ITEM_BACKSTAGE_PASS:
+				item.setQuality(0);
+				break;
+			case ITEM_LEGENDARY:
+				break;
+			default:
+				if (item.getQuality() > 0) {
+					item.setQuality(item.getQuality() - 1);
+				}
+				break;
+		}
+	}
+
+	private void increaseQuality(Item item, int amount) {
+		int newQuality = Math.min(50, item.getQuality() + amount);
+		item.setQuality(newQuality);
 	}
 
 	@Override
 	public String toString() {
 		return Arrays.toString(items);
 	}
-
 }
